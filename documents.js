ff440fd844ea21b472062507a4ab191b45510f2c
lost-manul <571042896@qq.com>
2019-10-09 13:38:36 +0800

    简陋的文档查看页面

---
+++ documents.js
@@ -0,0 +1,31 @@
+let documents = [
+  {
+    title: 'CentOS系统tomcat启动太慢',
+    tags: [
+      'tomcat'
+    ],
+    path: '1910/00.md'
+  },
+  {
+    title: 'IDEA使用Lombok插件后编译报错',
+    tags: [
+      'IDEA'
+    ],
+    path: '1910/01.md'
+  },
+  {
+    title: 'nginx通过申请Let\'s Encrypt免费SSL证书来使用https',
+    tags: [
+      'nginx',
+      'https'
+    ],
+    path: '1910/02.md'
+  },
+  {
+    title: 'vue-cli配置文件详解',
+    tags: [
+      'vue'
+    ],
+    path: '1910/03.md'
+  }
+]
---
+++ tree.js
@@ -0,0 +1,37 @@
+let tree = [
+  {
+    title: '前端',
+    children: [
+      {
+        title: 'vue'
+      }
+    ]
+  },
+  {
+    title: '开发工具',
+    children: [
+      {
+        title: 'IDEA'
+      }
+    ]
+  },
+  {
+    title: '部署工具',
+    children: [
+      {
+        title: 'nginx'
+      },
+      {
+        title: 'tomcat'
+      }
+    ]
+  },
+  {
+    title: '其他',
+    children: [
+      {
+        title: 'https'
+      }
+    ]
+  }
+]
